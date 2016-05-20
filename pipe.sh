0f59625ada813d65997f38f530616bb610e956e1
Nicholas D. Socci <soccin@mskcc.org>
2016-05-20 19:44:12 -0400

    Added bSyncs and STAGE-ing tests

--- pipe.sh
+++ pipe.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#./PEMapper/pipe.sh eColi_MG1665 /ifs/archive/GCL/hiseq/FASTQ/VIC_2219_000000000-AGKGN/Project_4298_B/Sample_BW30270
-#./PEMapper/pipe.sh eColi_MG1665 /ifs/archive/GCL/hiseq/FASTQ/VIC_2219_000000000-AGKGN/Project_4298_B/Sample_K761ER765E
-
 PROJNO=proj_04298_D
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
@@ -13,9 +10,10 @@ JAVA=/opt/common/CentOS_6/java/jdk1.7.0_75/bin/java
 GATKJAR=/opt/common/CentOS_6/gatk/GenomeAnalysisTK-3.4-0-g7e26428/GenomeAnalysisTK.jar
 GENOME=/ifs/work/socci/Depot/Genomes/E.coli/K12/MG1655/eColi__MG1655.fa
 
-if [ "" ]; then
-echo "Do not re-run"
-exit
+STAGE1="YES"
+STAGE2="YES"
+
+if [ "$STAGE1" == "YES" ]; then
 
 bsub -o LSF/ -n 24 -J STAGE_1_$$ -R "rusage[mem=72]" -We 59 \
     $SDIR/haplotypeCallerDMP.sh \
@@ -26,12 +24,10 @@ bsub -o LSF/ -n 24 -J STAGE_1_$$ -R "rusage[mem=72]" -We 59 \
     $GENOME \
     NC_000913 \
     $BAMS
+
 fi
 
-#HOLDID=$(~/bin/qstatFull | awk -v TAG=STAGE_1_$$ '$3==TAG{print $1}' | xargs  | tr ' ' ',')
-#echo HOLDID=$HOLDID
-#qSYNC $HOLDID
-#sleep 60
+bSync STAGE_1_$$
 
 INPUTS=$(find output -name "*.bam" | awk '{print "-I "$1}')
 KNOWN=$(find output -name "*.vcf")
@@ -39,16 +35,15 @@ KNOWN=$(find output -name "*.vcf")
 echo KNOWN=$KNOWN
 echo INPUTS=$INPUTS
 
-if [ "" ]; then
-echo "Do not run"
-exit
+if [ "$STAGE2" == "YES" ]; then
+
 bsub -o LSF/ -n 24 -J STAGE_2_$$ -R "rusage[mem=72]" -We 59 \
 $JAVA -Xms256m -Xmx48g -XX:-UseGCOverheadLimit -Djava.io.tmpdir=/scratch/socci -jar $GATKJAR \
     -T BaseRecalibrator -nct 24 -R $GENOME -knownSites $KNOWN -o recal_data.table \
     $INPUTS
 
 for bam in $(find output -name "*.bam"); do
-bsub -o LSF/ -n 2 -J STAGE_3_$$ -R "rusage[mem=50]" -We 59 -w "post_done(STAGE_2_$$)" \
+bsub -o LSF/ -n 2 -J STAGE_2a_$$ -R "rusage[mem=50]" -We 59 -w "post_done(STAGE_2_$$)" \
     $JAVA -Xms256m -Xmx48g -XX:-UseGCOverheadLimit -Djava.io.tmpdir=/scratch/socci \
     -jar $GATKJAR \
     -T PrintReads -R $GENOME \
@@ -58,14 +53,12 @@ done
 
 fi
 
-#HOLDID=$(~/bin/qstatFull | awk -v TAG=STAGE_3_$$ '$3==TAG{print $1}' | xargs  | tr ' ' ',')
-#echo HOLDID=$HOLDID
-#qSYNC $HOLDID
-#sleep 60
+bSync STAGE_2a_$$
+
 
 INPUTS=$(find output -name "*recal.bam")
 #qsub -pe alloc 24 -N STAGE_4_$$ -l virtual_free=3G ~/Work/SGE/qCMD \
-    $SDIR/haplotypeCallerDMP.sh \
+$SDIR/haplotypeCallerDMP.sh \
     $GENOME $KNOWN NC_000913 $INPUTS
 
 
